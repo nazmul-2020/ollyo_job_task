5de8948fde3dded2ec5207c203113e9305b01a5a
nazmul-2020 <nazmulcpi2012@gmail.com>
2023-11-05 13:13:12 +0600

    task manual integrated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Img_gallery from './component/Img_gallery'
+
+function App() {
+
+  return (
+    <div className='mt-x mx-auto bg-slate-300  h-screen py-10'>
+
+     <Img_gallery></Img_gallery>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/Img_gallery.jsx
@@ -0,0 +1,19 @@
+
+const Img_gallery = () => {
+    return (
+        <div className="bg-white  my-10  ">
+            <div className="lg:w-1/2 md:w-3/4 w-full  mt-3 pt-3 mx-auto border-2 rounded-lg shadow-lg ">
+                <div className="flex items-center justify-between mx-2">
+                    <h1 className=" text-xl font-bold">Gallery</h1>
+                    <h2 className="font-medium text-red-500 cursor-pointer pb-2">Delete files</h2>
+                </div>
+                <hr />
+                <div className="gr">
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Img_gallery;
